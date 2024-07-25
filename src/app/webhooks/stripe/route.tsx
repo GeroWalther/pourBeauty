075163540cfdabb1be8d031449ba87f3d20c9757
ahmad-jq <nu@bondex.app>
2024-07-25 21:24:04 +0500

    Update route.tsx for vercel deployment test

--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -1,6 +1,6 @@
 // import { saveOrder } from '@/app/actions/orders';
 
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 // import Stripe from 'stripe';
 // import { Resend } from 'resend';
 
@@ -75,56 +75,7 @@
 //     //   react: <h1>Hi</h1>,
 //     // });
 //   }
-//   return new NextResponse();
-// }
-
-import { saveOrder } from '@/app/actions/orders';
-
-import { NextRequest, NextResponse } from 'next/server';
-import Stripe from 'stripe';
-// import { Resend } from 'resend';
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function POST(req: NextRequest) {
-  const event = stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get('stripe-signature') as string,
-    process.env.STRIPE_WEBHOOK_SECRET as string
-  );
-
-  if (event.type === 'charge.succeeded') {
-    const charge = event.data.object;
-    const productsAsString = charge.metadata.products;
-    const products = JSON.parse(productsAsString);
-
-    const email = charge.billing_details.email;
-    const pricePaidInCents = charge.amount;
-    // const customerName = charge.billing_details.name;
-    const city = charge.shipping?.address?.city;
-    const country = charge.shipping?.address?.country;
-    const line1 = charge.shipping?.address?.line1;
-    const line2 = charge.shipping?.address?.line2;
-    const postalCode = charge.shipping?.address?.postal_code;
-    const state = charge.shipping?.address?.state;
-    const address = `${line1}, ${
-      line2 ? line2 + ', ' : ''
-    } ${city}, ${state}, ${postalCode}, ${country}`;
-
-    if (email == null || address == null) {
-      return new NextResponse('Bad Request', { status: 400 });
-    }
-
-    //  Save order in DB
-    await saveOrder(address, email!, products, pricePaidInCents);
-    // send email to customer
-    // await resend.emails.send({
-    //   from: `Support <${process.env.SENDER_EMAIL}>`,
-    //   to: email,
-    //   subject: 'Order Confirmation',
-    //   react: <h1>Hi</h1>,
-    // });
-  }
   return new NextResponse();
 }
+
+
