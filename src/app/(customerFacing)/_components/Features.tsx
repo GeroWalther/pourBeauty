339bc5238dd8e6d8312ffe4f866611da4587cb23
GeroWalther <gero.walther@gmail.com>
2024-08-08 12:12:05 +0200

    featured section in english and german

--- src/app/(customerFacing)/_components/Features.tsx
+++ src/app/(customerFacing)/_components/Features.tsx
@@ -1,29 +1,45 @@
+'use client';
+import { useLanguage } from '@/contexts/LanguageProvider';
 import { Gem, InfinityIcon, LeafIcon, NutOffIcon, Rabbit } from 'lucide-react';
 import React from 'react';
-const features = [
-  {
-    icon: Rabbit,
-    title: 'Ohne Tierversuche',
-    text: 'Unsere Produkte werden ohne Tierversuche entwickelt und hergestellt.',
-  },
-  {
-    icon: LeafIcon,
-    title: 'Natürlich',
-    text: 'Wir verwenden nur natürliche Inhaltsstoffe in unseren Produkten.',
-  },
-  {
-    icon: Gem,
-    title: 'Hip und modern',
-    text: 'Unsere Produkte sind trendy und entsprechen den neuesten Beauty-Trends.',
-  },
-  {
-    icon: InfinityIcon,
-    title: 'Super Ergebnisse',
-    text: 'Unsere Kunden sind mit den Ergebnissen unserer Produkte sehr zufrieden.',
-  },
-];
 
 const FeaturesSection = () => {
+  const { language } = useLanguage();
+  const features = [
+    {
+      icon: Rabbit,
+      title: language == 'de' ? 'Ohne Tierversuche' : 'No animal testing.',
+      text:
+        language == 'de'
+          ? 'Unsere Produkte werden ohne Tierversuche entwickelt und hergestellt.'
+          : 'Our products are developed and manufactured without animal testing.',
+    },
+    {
+      icon: LeafIcon,
+      title: language == 'de' ? 'Natürlich' : 'Natural',
+      text:
+        language == 'de'
+          ? 'Wir verwenden nur natürliche Inhaltsstoffe in unseren Produkten.'
+          : 'We use only natural ingredients in our products.',
+    },
+    {
+      icon: Gem,
+      title: language == 'de' ? 'Hip und modern' : 'Trendy and modern',
+      text:
+        language == 'de'
+          ? 'Unsere Produkte sind trendy und entsprechen den neuesten Beauty-Trends.'
+          : 'Our products are trendy and align with the latest beauty trends.',
+    },
+    {
+      icon: InfinityIcon,
+      title: language == 'de' ? 'Super Ergebnisse' : 'Amazing results',
+      text:
+        language == 'de'
+          ? 'Unsere Kunden sind mit den Ergebnissen unserer Produkte sehr zufrieden.'
+          : 'Our customers are very satisfied with the results of our products.',
+    },
+  ];
+
   return (
     <div className='grid gap-8 md:grid-cols-2 lg:grid-cols-4 p-8 border border-t'>
       {features.map((feature, index) => (
