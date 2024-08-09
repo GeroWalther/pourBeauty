94a0aae4515e937630ecfae5f601cb618f375b82
GeroWalther <gero.walther@gmail.com>
2024-08-09 21:11:26 +0200

    lg

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -6,6 +6,9 @@ import FeaturesSection from './_components/Features';
 import ProductSectionMagicLips from './_components/ProductSectionMagicLips';
 
 export default function HomePage() {
+  console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+  console.log(process.env.STRIPE_SECRET_KEY);
+  console.log(process.env.STRIPE_WEBHOOK_SECRET);
   return (
     <>
       <HeroComp />
