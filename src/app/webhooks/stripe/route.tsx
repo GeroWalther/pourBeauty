4382a99ff1c04817a45a62d73a397810b06d9981
GeroWalther <gero.walther@gmail.com>
2024-08-10 15:10:11 +0200

    try catch in webhook

--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -11,88 +11,92 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
-  const event = stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get('stripe-signature') as string,
-    process.env.STRIPE_WEBHOOK_SECRET as string
-  );
-
-  if (event.type === 'charge.succeeded') {
-    const charge = event.data.object;
-  //  console .log(charge);
-    const productsAsString = charge.metadata.products;
-    const products = JSON.parse(productsAsString);
-    // console.log('PRODUCTS:', products);
-
-    const email = charge.billing_details.email;
-    const pricePaidInCents = charge.amount;
-    const customerName = charge.billing_details.name;
-
-    const city = charge.shipping?.address?.city;
-    const country = charge.shipping?.address?.country;
-    const line1 = charge.shipping?.address?.line1;
-    const line2 = charge.shipping?.address?.line2;
-    const postalCode = charge.shipping?.address?.postal_code;
-    const state = charge.shipping?.address?.state;
-    const address = `${line1}, ${
-      line2 ? line2 + ', ' : ''
-    } ${city}, ${state}, ${postalCode}, ${country}`;
-
-    if (email == null || address == null || customerName == null) {
-      return new NextResponse('Bad Request', { status: 400 });
-    }
-
-    //  Save order in DB
-    const { order } = await saveOrder(
-      address,
-      customerName,
-      email!,
-      products,
-      pricePaidInCents
+  try {
+    const event = stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get('stripe-signature') as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
     );
 
-    // email to admin
-    await resend.emails.send({
-      from: `BESTELLUNGSEINGANG <${process.env.SENDER_EMAIL}>`,
-      to: process.env.SHOP_EMAIL as string,
-      subject: 'Neue Bestellung eingegangen',
-      react: (
-        <PurchaseReceiptEmail
-          products={products}
-          order={{
-            id: order.id,
-            createdAt: order.createdAt,
-            customerName,
-            address,
-            email,
-            pricePaidInCents,
-            shippingCost: order.shippingCost,
-          }}
-        />
-      ),
-    });
+    if (event.type === 'charge.succeeded') {
+      const charge = event.data.object;
+      //  console .log(charge);
+      const productsAsString = charge.metadata.products;
+      const products = JSON.parse(productsAsString);
+      // console.log('PRODUCTS:', products);
 
-    // send email to customer
-    await resend.emails.send({
-      from: `Bestellung MISS GLOW BEAUTY <${process.env.SENDER_EMAIL}>`,
-      to: email,
-      subject: 'Vielen Dank für deine Bestellung',
-      react: (
-        <PurchaseReceiptEmail
-          isAdmin={false}
-          products={products}
-          order={{
-            id: order.id,
-            createdAt: order.createdAt,
-            customerName,
-            address,
-            email,
-            pricePaidInCents,
-            shippingCost: order.shippingCost,
-          }}
-        />
-      ),
-    });
+      const email = charge.billing_details.email;
+      const pricePaidInCents = charge.amount;
+      const customerName = charge.billing_details.name;
+
+      const city = charge.shipping?.address?.city;
+      const country = charge.shipping?.address?.country;
+      const line1 = charge.shipping?.address?.line1;
+      const line2 = charge.shipping?.address?.line2;
+      const postalCode = charge.shipping?.address?.postal_code;
+      const state = charge.shipping?.address?.state;
+      const address = `${line1}, ${
+        line2 ? line2 + ', ' : ''
+      } ${city}, ${state}, ${postalCode}, ${country}`;
+
+      if (email == null || address == null || customerName == null) {
+        return new NextResponse('Bad Request', { status: 400 });
+      }
+
+      //  Save order in DB
+      const { order } = await saveOrder(
+        address,
+        customerName,
+        email!,
+        products,
+        pricePaidInCents
+      );
+
+      // email to admin
+      await resend.emails.send({
+        from: `BESTELLUNGSEINGANG <${process.env.SENDER_EMAIL}>`,
+        to: process.env.SHOP_EMAIL as string,
+        subject: 'Neue Bestellung eingegangen',
+        react: (
+          <PurchaseReceiptEmail
+            products={products}
+            order={{
+              id: order.id,
+              createdAt: order.createdAt,
+              customerName,
+              address,
+              email,
+              pricePaidInCents,
+              shippingCost: order.shippingCost,
+            }}
+          />
+        ),
+      });
+
+      // send email to customer
+      await resend.emails.send({
+        from: `Bestellung MISS GLOW BEAUTY <${process.env.SENDER_EMAIL}>`,
+        to: email,
+        subject: 'Vielen Dank für deine Bestellung',
+        react: (
+          <PurchaseReceiptEmail
+            isAdmin={false}
+            products={products}
+            order={{
+              id: order.id,
+              createdAt: order.createdAt,
+              customerName,
+              address,
+              email,
+              pricePaidInCents,
+              shippingCost: order.shippingCost,
+            }}
+          />
+        ),
+      });
+    }
+    return new NextResponse();
+  } catch (error) {
+    console.error('Error:', error);
   }
-  return new NextResponse();
 }
