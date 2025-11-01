07c2d43e5dfa341f65c68a9dabe02009aceb8947
GeroWalther <gero.walther@gmail.com>
2025-11-01 19:23:03 +0100

    proddisp section updated.

--- src/app/(customerFacing)/_components/ProdDispComp.tsx
+++ src/app/(customerFacing)/_components/ProdDispComp.tsx
@@ -9,23 +9,18 @@ type pType = {
 
 const ProdDispComp = ({ name, productImg, link }: pType) => {
   return (
-    <div
-      className='flex items-center justify-center flex-auto h-80 hover:brightness-100 w-full transition-transform duration-300 transform hover:scale-110 md:rounded-md'
-      style={{
-        backgroundImage: `url(${productImg})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-      }}>
-      <div className='text-center flex flex-col items-center px-10'>
-        <h2 className='text-xl font-bold text-dark mb-5 text-gray-800'>
-          {name}
-        </h2>
-        <div className='flex justify-center items-center gap-5'>
-          <Link href={link} className={buttonVariants()}>
-            Shop
-          </Link>
-        </div>
+    <div className='group flex w-full flex-col overflow-hidden rounded-md transition-transform duration-300 transform hover:scale-110'>
+      <div
+        className='flex h-64 w-full items-center justify-center bg-cover bg-center bg-no-repeat brightness-95 transition duration-300 group-hover:brightness-100'
+        style={{
+          backgroundImage: `url(${productImg})`,
+        }}
+      />
+      <div className='flex flex-col items-center gap-4 bg-white px-10 py-6 text-center'>
+        <h2 className='text-xl font-bold text-gray-800'>{name}</h2>
+        <Link href={link} className={buttonVariants()}>
+          Shop
+        </Link>
       </div>
     </div>
   );
